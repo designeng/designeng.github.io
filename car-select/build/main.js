6a29d110b9d9ab8932103cf94ad0c4ab6fd42b5d
Denis Savenok <denissavenok@gmail.com>
2015-10-05 11:52:45 +0400

    path

--- car-select/build/main.js
+++ car-select/build/main.js
@@ -29856,7 +29856,7 @@ define('wire/lib/context',['require','when','./object','./loader/adapter','./loa
 define('buildAssets',["wire/lib/context", "wire/lib/WireContext", "wire/lib/scope", "wire/lib/plugin/registry", "wire/lib/plugin/defaultPlugins", "wire/lib/graph/DirectedGraph", "wire/lib/graph/trackInflightRefs", "wire/lib/Map", "wire/lib/ComponentFactory", "wire/lib/lifecycle", "wire/lib/resolver", "wire/lib/graph/cyclesTracker", "wire/lib/instantiate", "wire/lib/plugin/wirePlugin", "wire/lib/plugin/basePlugin", "wire/lib/graph/tarjan", "wire/lib/graph/formatCycles", "wire/lib/ObjectProxy", "wire/lib/invoker", "wire/lib/pipeline"], function() {});
 
 require.config({
-  baseUrl: "car-select/js",
+  baseUrl: "/car-select/js",
   paths: {
     "text": "lib/text"
   },
